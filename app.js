22036cc2207b13e29e35cf1bf07a7fb46d8939a2
Umer Farooq <Umer Farooq>
2016-07-20 16:29:41 +0500

    Tinkering with Cors in app.js

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ var cors = require('cors');
 var app = express();
 app.use(cors);
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
