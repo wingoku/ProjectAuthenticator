daafb88a65a2d329c0d11927dc268360cbadcf04
Umer Farooq <Umer Farooq>
2016-07-20 14:58:53 +0500

    First commit: Authenticator added to index.js, Cors added index.js

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var cors = require('cors');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+router.get('/authenticate/:appName', cors(), function(req, res, next) {
+  console.log("app to authenticate: "+ req.params.appName);
+  if(req.params.appName === "PrinterApp") {
+    res.json({'result':true});
+  }
+});
+
+module.exports = router;
