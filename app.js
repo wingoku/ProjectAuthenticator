5ad6a5b7185fdfd5a7d8c95e6886ce434e4cc769
Umer Farooq <Umer Farooq>
2016-07-20 16:39:14 +0500

    Removed: cors library app.use(cors) from app.js

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ var users = require('./routes/users');
 var cors = require('cors');
 
 var app = express();
-app.use(cors);
 
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+//app.use(cors);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
